c52c762296e95a977b1687d7aca285604c11fe5a
Georgy <geovo851@gmail.com>
2015-12-19 04:41:08 -0800

    Test pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,10 @@ FactoryGirl.define do
   factory :category do
     title 'Men\'s Outerwear'
   end
+
+  factory :product do 
+    name 'Double Breasted Pocket Hemming'
+    price 75
+    category
+  end
 end
--- spec/requests/products_pages_spec.rb
+++ spec/requests/products_pages_spec.rb
@@ -18,16 +18,40 @@ describe 'Products pages' do
     it { should_not have_title('Contact Us |') }
   end
 
-  describe 'categories' do
+  describe 'products' do
     let!(:category) { FactoryGirl.create(:category) }
-    before do
-      visit root_path
-      click_link category.title
+
+    describe 'category' do
+      before do
+        visit root_path
+        click_link category.title
+      end
+
+      context 'when category is found' do 
+        it { expect(page).to have_selector('h2', text: category.title) }
+      end
     end
 
-    context 'should category' do 
-      it { expect(page).to have_selector('h2', text: category.title) }
+    describe 'pagination' do
+      context 'when pagination is found' do
+        before do
+          15.times { FactoryGirl.create(:product, category: category) }
+          visit root_path
+          click_link category.title
+        end
+        it { expect(page).to have_css('.pagination') }
+      end
+
+      context 'when pagination is not found' do
+        before do
+          1.times { FactoryGirl.create(:product, category: category) }
+          visit root_path
+          click_link category.title
+        end
+        it { expect(page).to_not have_css('.pagination') }
+      end
     end
+
   end
 
 end
